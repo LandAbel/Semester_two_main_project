6688c4f745d36b5b2ffe21d785c3e690893cf2fd
LandAbel <landiabel4@gmail.com>
2022-07-06 19:46:57 +0200

    main project

---
+++ Semester_two_main_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semester_two_main_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
